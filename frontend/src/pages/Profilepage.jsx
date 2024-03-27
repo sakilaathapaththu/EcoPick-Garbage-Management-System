fddab2cc9e81e80d61ce18e30fe651eb887f7653
Athapaththu <it21326240@my.sliit.lk>
2024-03-27 10:02:07 +0530

    create profile page

--- frontend/src/pages/Profilepage.jsx
+++ frontend/src/pages/Profilepage.jsx
@@ -12,7 +12,8 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import testpic from "../assets/images/profile.jpeg";
-
+import DeleteIcon from "@mui/icons-material/Delete";
+import TextField from '@mui/material/TextField';
 
 export default function Profilepage() {
   const [user, setUser] = useState(null);
@@ -41,60 +42,140 @@ export default function Profilepage() {
     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
     padding: theme.spacing(1),
-    textAlign: "center",
+
     color: theme.palette.text.secondary,
   }));
   return (
     <div>
-      <br></br>
-      <br></br>
-      <br></br>
-      {user && (
-        <div>
-          <h2>Welcome, {user.firstName}</h2>
-          <p>Email: {user.email}</p>
-          {/* Add form fields to update user details */}
-        </div>
-      )}
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
+      <Box sx={{ flexGrow: 1, mt: 15 }}>
+        <Grid container spacing={1}>
           <Grid item xs={1}></Grid>
           <Grid item xs={4}>
-            <Card sx={{ maxWidth: 345 }}>
+            <Card sx={{ maxWidth: 400 }}>
               <CardMedia
                 component="img"
                 alt="Profile Image"
-                
-                
                 maxWidth="100"
                 image={testpic}
                 sx={{
-                 
-                
-                  
-                
-                  borderRadius: "100%", // Shape the image as a circle
+                  mt: 5,
+                  width: 150, // Adjust the width as needed
+                  height: 150, // Adjust the height as needed
+                  display: "block", // Ensure the image is treated as a block element
+                  ml: "auto",
+                  mr: "auto", // Center the image horizontally
+                  borderRadius: "50%", // Shape the image as a circle
                   overflow: "hidden", // Hide overflow to maintain shape
                 }}
               />
+
               <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
-                </Typography>
+                {user && (
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    sx={{ textAlign: "center" }} // Align text center horizontally
+                  >
+                    {user.firstName} {user.lastName}
+                  </Typography>
+                )}
+                {user && (
+                  <Typography
+                    gutterBottom
+                    variant="h6"
+                    component="div"
+                    sx={{ textAlign: "center" }}
+                  >
+                    {user.email}
+                  </Typography>
+                )}
               </CardContent>
-              <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
+              <CardActions sx={{ justifyContent: "center" }}>
+                <Button variant="contained" color="success">
+                  Edit
+                </Button>
+                <Button
+                  variant="outlined"
+                  startIcon={<DeleteIcon />}
+                  color="error"
+                >
+                  Delete
+                </Button>
               </CardActions>
             </Card>
           </Grid>
 
           <Grid item xs={6}>
-            <Item>xs=4</Item>
+          {user && (
+            <Item>
+              
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  sx={{}} // Align text center horizontally
+                >
+                  Welcome, {user.firstName} {user.lastName}
+                </Typography>
+              
+
+              <TextField
+                fullWidth
+                id="firstName"
+                label="First Name"
+                variant="outlined"
+                margin="normal"
+                value={user.firstName}
+                onChange={(e) =>
+                  setUser({ ...user, firstName: e.target.value })
+                }
+              />
+
+              <TextField
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                variant="outlined"
+                margin="normal"
+                value={user.lastName}
+                onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+              />
+
+              <TextField
+                fullWidth
+                id="email"
+                label="Email"
+                variant="outlined"
+                margin="normal"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+              />
+
+              <TextField
+                fullWidth
+                id="contactNo"
+                label="Contact No"
+                variant="outlined"
+                margin="normal"
+                value={user.contactNo}
+                onChange={(e) =>
+                  setUser({ ...user, contactNo: e.target.value })
+                }
+              />
+
+              <TextField
+                fullWidth
+                id="address"
+                label="Address"
+                variant="outlined"
+                margin="normal"
+                value={user.address}
+                onChange={(e) => setUser({ ...user, address: e.target.value })}
+              />
+             
+            </Item>
+            )}
           </Grid>
           <Grid item xs={1}></Grid>
 
